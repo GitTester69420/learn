25dd0aae5e7312cb8c6b26ba11888d6393f1c656
dlaw9 <dlaw9@DESKTOP-GHFC8RL.cgocable.net>
2022-01-26 11:32:56 -0500

    Second commit

---
+++ src/learn/Person.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package learn;
+
+/**
+ *
+ * @author dlaw9
+ */
+public class Person {
+    private String name;
+    
+    public Person(){
+        
+    }
+    
+}
