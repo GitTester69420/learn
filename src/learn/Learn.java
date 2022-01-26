95d571686a272cd128bc57c39904eae69b9e5659
lawdy@sheridancollege.ca <lawdy@sheridancollege.ca>
2022-01-26 12:07:23 -0500

    testing new user

--- src/learn/Learn.java
+++ src/learn/Learn.java
@@ -17,6 +17,7 @@ public class Learn {
     public static void main(String[] args) {
         
         System.out.println("Welcome!");
+        System.out.println("Testing after changes");
     }
     
 }
