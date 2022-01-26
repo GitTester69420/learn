1128ca19c628d6f5c57a3a4f9fe01ea1821bdac5
lawdy@sheridancollege.ca <dlaw96@hotmail.com>
2022-01-26 11:43:02 -0500

    Commit from new computer

--- src/learn/Learn.java
+++ src/learn/Learn.java
@@ -15,7 +15,7 @@ public class Learn {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         System.out.println("Welcome!");
     }
     
